fa129a95d3ba2326b79aa317f0fd41793e19c1f1
Deepjyoti Paul <deepjyotipaul69@gmail.com>
2022-09-17 14:42:52 +0530

    gamification rest api

---
+++ gamification/api/urls.py
@@ -0,0 +1,14 @@
+from .views import ObjectsView,PointsView,UserView
+from rest_framework.routers import DefaultRouter
+from rest_framework.urls import path
+from django.urls import include
+
+router=DefaultRouter()
+
+router.register("users", UserView, basename='users')
+router.register("objects", ObjectsView, basename='objects')
+router.register("points", PointsView, basename='points')
+
+urlpatterns=[
+    path("", include(router.urls)),
+]
